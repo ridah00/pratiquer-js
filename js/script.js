b6a19f36511d97baddb423f1fe9d823760d75d86
ridha_mahdjoub <mahdjoubridah00@gmail.com>
2024-12-11 10:29:32 +0100

    bouton_reset

--- js/script.js
+++ js/script.js
@@ -15,6 +15,30 @@ function text_to_grey() {
   document.getElementById("text_output").classList.remove("text-success", "text-danger", "text-secondary");
   document.getElementById("text_output").classList.add("text-secondary");
 }
+function text_to_grey() {
+  document.getElementById("text_output").classList.remove("text-success", "text-danger", "text-secondary");
+  document.getElementById("text_output").classList.add("text-secondary");
+}
+function text_to_origin(){
+  document.getElementById("text_input").value=null;
+  document.getElementById("text_output").innerText="";
+  document.getElementById("text_output").classList.remove("text-success", "text-danger", "text-secondary");
+}
+document.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("go_live").click();
+    }
+});
+
+document.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 27) {
+      document.getElementById("reset_button").click();
+  }
+});
+
+
 // function changer_couleur(color_) {
 //   document.getElementById("text_output").style.color = color_;
 // }
